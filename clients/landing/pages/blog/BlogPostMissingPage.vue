6eb978c8d578c6d64e5d1e6c584a2e23b8cd61fc
Scott Erickson <sderickson@gmail.com>
2025-03-25 19:21:32 -0700

    Use more vuetify

--- clients/landing/pages/blog/BlogPostMissingPage.vue
+++ clients/landing/pages/blog/BlogPostMissingPage.vue
@@ -1,40 +1,15 @@
 <template>
-  <div class="missing-post">
-    <h1>Blog Post Not Found</h1>
-    <p>Sorry, we couldn't find the blog post you're looking for.</p>
-    <router-link to="/blog" class="back-link">
-      Return to Blog List
-    </router-link>
-  </div>
+  <v-container>
+    <v-row justify="center" align="center" class="fill-height">
+      <v-col cols="12" sm="8" md="6" class="text-center">
+        <h1 class="text-h3 mb-4">Blog Post Not Found</h1>
+        <p class="text-body-1 text-medium-emphasis mb-6">
+          Sorry, we couldn't find the blog post you're looking for.
+        </p>
+        <v-btn color="primary" :to="'/blog'" prepend-icon="mdi-arrow-left">
+          Return to Blog List
+        </v-btn>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
-
-<style scoped>
-.missing-post {
-  text-align: center;
-  padding: 3rem 1rem;
-}
-
-h1 {
-  color: #2c3e50;
-  margin-bottom: 1rem;
-}
-
-p {
-  color: #666;
-  margin-bottom: 2rem;
-}
-
-.back-link {
-  display: inline-block;
-  padding: 0.75rem 1.5rem;
-  background-color: #42b983;
-  color: white;
-  text-decoration: none;
-  border-radius: 4px;
-  transition: background-color 0.2s;
-}
-
-.back-link:hover {
-  background-color: #3aa876;
-}
-</style>
--- clients/landing/pages/blog/BlogPostPage.vue
+++ clients/landing/pages/blog/BlogPostPage.vue
@@ -1,19 +1,15 @@
 <template>
-  <div class="blog-post">
-    <router-view v-slot="{ Component }">
-      <component :is="Component" />
-    </router-view>
-  </div>
+  <v-container>
+    <v-row justify="center">
+      <v-col cols="12" md="10" lg="8">
+        <router-view v-slot="{ Component }">
+          <component :is="Component" />
+        </router-view>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script setup lang="ts">
 // This component acts as a wrapper for dynamic blog post content
 </script>
-
-<style scoped>
-.blog-post {
-  padding: 2rem;
-  max-width: 800px;
-  margin: 0 auto;
-}
-</style>
